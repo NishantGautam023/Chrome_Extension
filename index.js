df831e83d8ea9974d6d0455a1b6502bacac0f6d5
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2021-05-11 17:32:19 +0000

    Initial 1.0

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-let myLeads = []
+// HTML selectors
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    // 2. Call the renderLeads() function
-    renderLeads()
-})
 
-// 1. Wrap the code below in a renderLeads() function
+// when user clicks on the save it shoud be saved on this array
+let myLeads = []
+
+// Click eventListener
+inputBtn.addEventListener("click", function() {
+    
+    myLeads.push(inputEl.value);  // push whatever we type in the box  into the array
+    
+    inputEl.value = "";  // clear out the input field
+    renderLeads()  // Display the text by calling the renderLeads() function
+})
+    
+
+// Function to display the text 
 function renderLeads() {
-    let listItems = ""
+    let listItems = ""  // instead of Manipulating DOM in the for loop adding it to texts itself
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        // listItems += "<li>" + "<a href='' traget='_blank'>" + myLeads[i] +"</a>"  + "</li>"
+        listItems += `
+        <li> <a target="_blank" href=" ${myLeads[i]}"> ${myLeads[i]} </a> </li> `
     }
+    // Rendering the texts
     ulEl.innerHTML = listItems  
 }
 
