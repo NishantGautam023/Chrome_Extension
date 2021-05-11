c0076758d19f5486d9fff6cf8a3f3f439a1762c7
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2021-05-11 15:43:31 +0000

    List Functionality

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-// HTML selectors
-
-const inputEl  = document.getElementsByClassName("input-btn")
-const saveEl = document.getElementsByClassName("btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    // 2. Call the renderLeads() function
+    renderLeads()
+})
 
-// when user clicks on the save it shoud be saved on this array.
-let myLeads = [] 
-
-// Click eventListener
- saveEl.addEventListener("click", function() {
-     // push whatever we type in the box  into the array
-     myLeads.push(inputEl.value)
-     
-     // Display the text by calling the renderText function
-     renderText()
- })
-
-
-// Function to display the text 
-function renderText() {
-    let texts = "" // instead of Manipulating DOM in the for loop adding it to texts itself
-    for ( let i =0; i < myLeads.length; i++ ) {
-        texts += "<li>" +  myLeads[i] + "</li>" 
+// 1. Wrap the code below in a renderLeads() function
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
     }
-    // Rendering the texts
-    ulEl.innerHTML = texts;
-
+    ulEl.innerHTML = listItems  
 }
+
