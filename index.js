66de57c9c0fa4d162cc07b856d112c6f6c07b4e7
Nishant Gautam <47427428+NishantGautam023@users.noreply.github.com>
2021-05-11 10:48:21 +0000

    Basic adding logic

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+// HTML selectors
+
 const inputEl  = document.getElementsByClassName("input-btn")
 const saveEl = document.getElementsByClassName("btn")
+const ulEl = document.getElementById("ul-el")
 
 
 // when user clicks on the save it shoud be saved on this array.
-let myLeads = []
+let myLeads = [] 
+
+// Click eventListener
+ saveEl.addEventListener("click", function() {
+     // push whatever we type in the box  into the array
+     myLeads.push(inputEl.value)
+     
+     // Display the text by calling the renderText function
+     renderText()
+ })
 
 
+// Function to display the text 
+function renderText() {
+    let texts = "" // instead of Manipulating DOM in the for loop adding it to texts itself
+    for ( let i =0; i < myLeads.length; i++ ) {
+        texts += "<li>" +  myLeads[i] + "</li>" 
+    }
+    // Rendering the texts
+    ulEl.innerHTML = texts;
 
-
-saveEl.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    console.log(myLeads);
-})
+}
